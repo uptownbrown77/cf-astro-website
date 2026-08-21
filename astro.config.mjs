87c230f2b05a496f2d023d9df0e6fdbd53a8e30b
uptownbrown77 <109543289+uptownbrown77@users.noreply.github.com>
2026-08-21 14:55:32 -0500

    Add files via upload

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,11 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
-
-
 import react from '@astrojs/react';
 
-
-const isBuild = process.argv[2] === 'build';
-
 export default defineConfig({
-  site: 'https://mpihj3-custos-fid-6b052a81-bsbrown77.wix-site-host.com',
+  site: 'https://wix-site-host.com',
   integrations: [react(), sitemap()],
   security: { checkOrigin: false },
-  ...(isBuild && { adapter: cloudProviderFetchAdapter({}) }),
-  image: { domains: ['static.wixstatic.com'] },
+  image: { domains: ['://wixstatic.com'] },
   output: 'server'
 });
